bb5b558c39892cbc7ff1a0a78cd1e49761851279
Joshua Kleckner <lokua@lokua.net>
2017-02-13 16:23:31 -0600

    ...

--- rollup.config.js
+++ rollup.config.js
@@ -1,18 +1,10 @@
 import buble from 'rollup-plugin-buble'
-import uglify from 'rollup-plugin-uglify'
-
-let dest = `dist/bundle.umd.js`
-const plugins = [buble()]
-
-if (process.env.NODE_ENV === `production`) {
-  plugins.push(uglify())
-  dest = `dist/bundle.umd.min.js`
-}
+const pkg = require(`./package.json`)
 
 export default {
   entry: `src/index.js`,
-  dest,
-  plugins,
+  dest: `dist/styled-ax.umd.min.js`,
+  plugins: [buble()],
   format: `umd`,
   moduleName: `styledAx`
 }
