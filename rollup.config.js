a48deef585183ff66c684aecd3608418c8dd34dd
Joshua Kleckner <lokua@lokua.net>
2017-02-13 18:53:11 -0600

    Use uglify

--- rollup.config.js
+++ rollup.config.js
@@ -1,10 +1,11 @@
 import buble from 'rollup-plugin-buble'
+import uglify from 'rollup-plugin-uglify'
 const pkg = require(`./package.json`)
 
 export default {
   entry: `src/index.js`,
   dest: `dist/styled-ax.umd.min.js`,
-  plugins: [buble()],
+  plugins: [buble(), uglify()],
   format: `umd`,
   moduleName: `styledAx`
 }
